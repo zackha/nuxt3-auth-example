8b11a16cce95c7b8469dfaaef2b62ccdbe9c3072
Zack Hatlen <zckhtln@icloud.com>
2023-03-21 09:36:54 +0300

    cleans it up

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,6 @@ const currentUser = useAuthUser()
 
 <template>
   <div>
-    <PageTitle title="Home" />
-    <PageDescription description="Simple authentification example using Nuxt3" />
     <PageUser :user="currentUser" />
   </div>
 </template>
